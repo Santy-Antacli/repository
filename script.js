98edf639f1a4234b2dcc20db6680de9802100ae1
Santiago Antacli <santy_antacli07@hotmail.com>
2020-09-07 10:19:44 -0300

    updated js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function alerta() {
     var res = document.getElementById("respuesta").value;
 
     while (res != 8 && cont < 4) {
-        alert("Su respuesta es incorrecta.")
+        alert("Su respuesta es incorrecta.");
         cont++; // No funciona el contador.
         break;
     }
 
-    if (cont >= 4) alert("No tiene más intentos. La respuesta correcta era 8.")
+    if (cont >= 4) alert("No tiene más intentos. La respuesta correcta era 8.");
 
-    if (res == 8) alert("La respuesta correcta es 8.");
+    if (res == 8) alert("Felicitaciones. La respuesta correcta es 8.");
 }
